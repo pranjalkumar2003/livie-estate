2e65dcae079b9b4a5fcaf7dc248a8c5d2bbcac01
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-01-30 22:46:03 +0530

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,15 +2,18 @@ import { useSelector } from "react-redux";
 import { useRef , useState, useEffect}  from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from "../firebase";
+import { updateUserStart,updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { useDispatch } from "react-redux";
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const {currentUser} = useSelector((state) => state.user);
+  const {currentUser, loading, error} = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUplaodError] = useState(false);
   const [formData, setFormData] = useState({});
- 
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
   //firebase storage 
   // allow read;
@@ -47,10 +50,38 @@ export default function Profile() {
      }
      );
   };
+
+   const handleChange = (e) => {
+       setFormData({ ...formData, [e.target.id]: e.target.value });
+   };
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+   }
   return (
     <div className='p-3 max-w-lg mx-auto' >
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col  gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col  gap-4'>
 
         <input onChange={(e)=>setFile(e.target.files[0])} 
         type="file" 
@@ -77,23 +108,37 @@ export default function Profile() {
           )}
         </p>
 
-        <input type="text" placeholder='User name'
-        className='border p-3 rounded-lg' id='username'/>
+        <input type="text" 
+        placeholder='User name'
+        defaultValue={currentUser.username}
+        className='border p-3 rounded-lg'
+        onChange={handleChange} 
+        id='username'/>
 
-        <input type="email" placeholder='Email address'
-         className='border p-3 rounded-lg' id='email'/>
+        <input type="email" 
+        placeholder='Email address'
+        defaultValue={currentUser.email}
+        className='border p-3 rounded-lg'
+        onChange={handleChange} 
+        id='email'/>
 
-       <input type="password" placeholder='Password'
-        className='border p-3 rounded-lg' id='password'/>
+       <input type="password" 
+       placeholder='Password'
+        className='border p-3 rounded-lg'
+        onChange={handleChange} 
+        id='password'/>
 
-        <button className='bg-slate-700 text-white rounded-lg p-3 
-        uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 
+        uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
 
       </form>
       <div className="flex justify-between mt-5">
       <span className='text-red-700 cursor-pointer'>Delete Account</span>
       <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess ? 'User updated successfully!' : ''}
+      </p>
     </div>
   )
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-
 const initialState = {
     currentUser: null,
     error: null,
@@ -19,16 +18,37 @@ const userSlice = createSlice({
             state.currentUser = action.payload;
             state.loading = false;
             state.error = null;
-
         },
 
         signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
-        }
-    }
+        },
+
+        updateUserStart: (state) => {
+            state.loading = true;
+        },
+
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+
+        updateUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },    
+    },
 });
 
-export const { signInStart, signInSucess, signInFailure } = userSlice.actions;
+export const { 
+    signInStart, 
+    signInSucess, 
+    signInFailure, 
+    updateUserFailure,
+     updateUserSuccess, 
+     updateUserStart
+ } = userSlice.actions;
 
 export default userSlice.reducer;
