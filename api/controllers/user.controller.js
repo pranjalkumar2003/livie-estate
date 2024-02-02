b955ab9d1a0e80bdb533bc9e919e0f64b280c845
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-03 02:01:53 +0530

    add contact landlord functionality to the listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -64,4 +64,19 @@ export const test = (req, res)=> {
     else{
       return next(errorHandler(401, 'You can only view your own listings!'))
     }
-   }
+   };
+
+   export const getUser = async (req, res, next) => {
+    try {
+  
+      const user = await User.findById(req.params.id);
+  
+      if (!user) return next(errorHandler(404, 'User not found!'));
+  
+      const { password: pass, ...rest } = user._doc;
+  
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
