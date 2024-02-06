5b8838d74f255d83d06bc052bded8e302504f16e
Pranjal Kumar Singh <pranjalkumarsingh4@gmail.com>
2024-02-06 17:38:25 +0530

    add ;

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+});
 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
